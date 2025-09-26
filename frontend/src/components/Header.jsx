fa1dd0cd0352d829c927caf3766c6d8c532094b3
janghyunjoon <135941433+janghyunjoon@users.noreply.github.com>
2025-09-26 15:03:19 +0900

    .

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Header.css";
 
+// 🚨 1. assets 폴더의 logo.svg 파일을 import 합니다.
+import LogoSvg from './../assets/logo.svg'; 
+
 function Header() {
   return (
     <header>
       <div className="logo">
-        <h1>자동차AI</h1>
+        {/* 🚨 2. <h1> 태그를 <img> 태그로 대체합니다. */}
+        <img src={LogoSvg} alt="자동차AI 로고" className="logo-img" />
       </div>
 
       <div className="login">
