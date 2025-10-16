5012c707d8f20964a8e1f68245db701898b71ca1
janghyunjoon <135941433+janghyunjoon@users.noreply.github.com>
2025-10-16 18:44:25 +0900

    .

--- frontend/src/components/ChatbotButton.jsx
+++ frontend/src/components/ChatbotButton.jsx
@@ -4,9 +4,15 @@ import "./ChatbotButton.css";
 function ChatbotButton() {
   return (
     <div className="chatbot">
-        <button>A.I 상담 시작</button>
-      </div>
+      <a 
+        href="https://sole-mighty-seriously.ngrok-free.app/webhook/82edfa8d-d7c2-4401-91c9-9f4b57d88529/chat" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+      >
+        챗봇 시작하기
+      </a>
+    </div>
   )
 }
 
-export default ChatbotButton
+export default ChatbotButton;
