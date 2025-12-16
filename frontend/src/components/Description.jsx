d55dc48e1684619bb5a02c77fe4d6073bb371674
janghyunjoon <135941433+janghyunjoon@users.noreply.github.com>
2025-12-17 08:45:59 +0900

    .

--- frontend/src/components/Description.jsx
+++ frontend/src/components/Description.jsx
@@ -13,10 +13,13 @@ const Hero = () => {
         <Oxquiz />
       </h1>
       <p className="txt">
-        AI에게 질문해 보세요
-        </p>
+        교통사고 발생 시 상황을 분석하여<br/>
+        도로교통법, 과실비율 인정기준, 판례 데이터 등을 종합분석하여<br/>
+        객관적인 과실비율을 제시하는 서비스입니다.<br/></p>
         <p className='engtxt'>
-        사고 경험이 없어도 누구나 이용가능하도록 준비했어요
+        We'll analyze the situation in the event of a traffic accident<br/>
+By comprehensively analyzing the Road Traffic Act, the criteria for recognizing the percentage of negligence, and case law data<br/>
+It is a service that presents an objective percentage of negligence.<br/>
       </p>
     </div>
   )
